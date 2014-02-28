22731f1a12618a8b8febb2948da13bbd642f9a00
WavingCookie <wavingrainbow@live.com>
2014-02-28 16:59:17 +0100

    JAPOON
    Gna gna

---
+++ main.lua
@@ -0,0 +1,14 @@
+-- Chargement des ressources
+function love.load() {
+    
+}
+ 
+-- Initilialisation de la congolexicomatisation
+function love.draw() {
+    
+}
+        
+-- Mise à jour
+function love.update(dt) {
+    
+}
