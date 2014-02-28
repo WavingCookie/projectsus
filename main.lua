c9356bd5130df2443df058d0d13006068981fefe
WavingCookie <wavingrainbow@live.com>
2014-02-28 17:20:33 +0100

    Ma bite

--- main.lua
+++ main.lua
@@ -1,14 +1,27 @@
 -- Chargement des ressources
 function love.load()
     player = love.graphics.newImage("resources/marioright.png")
+    playerx = 50
+    playery = 50
+    music = love.audio.newSource("music.mp3", "stream")
+    love.audio.play(music)
 end
  
 -- Initilialisation de la congolexicomatisation
 function love.draw()
-    love.graphics.draw(player, 50, 50)
+        love.graphics.draw(player, playerx, playery)
 end
         
 -- Mise à jour
 function love.update(dt)
-    
+    function love.keypressed(key)
+        if key == "Z" then
+            playery = playery+8
+        end
+    end
+    function love.keypressed(key)
+        if key == "Q" then
+            playerx = playerx-8
+        end
+    end    
 end
