ed3203c32b6f87cb50cccde3e6723cedc3d90517
WavingCookie <wavingrainbow@live.com>
2014-02-28 17:26:07 +0100

    son histoire

--- main.lua
+++ main.lua
@@ -5,15 +5,15 @@ function love.load()
     playery = 50
     music = love.audio.newSource("music.mp3", "stream")
     love.audio.play(music)
-end
- 
--- Initilialisation de la congolexicomatisation
-function love.draw()
-        love.graphics.draw(player, playerx, playery)
+    currentos = love.system.getOS()
 end
         
 -- Mise à jour
 function love.update(dt)
+    function love.draw()
+        love.graphics.draw(player, playerx, playery)
+        love.graphics.print(currentos, 16, 16)
+    end
     function love.keypressed(key)
         if key == "Z" then
             playery = playery+8
